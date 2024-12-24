7b3f7c53064772c09d93b88285f64878040da99b
Nukri Tskvitaia <nukritskvitaia@gmail.com>
2024-12-24 11:26:39 +0400

    Add project files.

---
+++ ClassesAndObjectsTask/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+
+namespace ClassesAndObjectsTask
+{
+    public class Employee
+    {
+        private string surname;
+        private int age;
+
+        public Employee()
+        {
+        }
+
+        public Employee(string surname, int age)
+        {
+            this.surname = surname;
+            this.age = age;
+        }
+
+        public void SetSurname(string surname)
+        {
+            this.surname = surname;
+        }
+
+        public string GetEmployeeInfo()
+        {
+            return $"Surname: {this.surname}, Age: {this.age}";
+        }
+
+        private string GetAge()
+        {
+            return this.age.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
